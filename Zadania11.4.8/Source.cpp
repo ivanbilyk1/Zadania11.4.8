49f7beffbe1709980d695d4d480e30d21f582f6d
Ivan Bilyk <thinkingyyy@gmail.com>
2024-05-14 23:50:24 +0300

    Tu som vytvoril 4 funkcie, prvé dve slúžia na presun kurzora doľava a doprava. Ďalšie dve slúžia na nastavenie hodnôt uzlu (vľavo a vpravo)

--- Zadania11.4.8/Source.cpp
+++ Zadania11.4.8/Source.cpp
@@ -32,6 +32,79 @@ char lowl_interpolate_linear(LOWL* list) {
 	return 'U';//Uspeh
 }
 
+char lowl_cur_step_left(LOWL* list) {
+	if (list == NULL || list->zac == NULL || list->potocny == NULL || list->potocny == list->zac) {
+		return 'P';//kurzor je už na začiatku alebo je zoznam prázdny
+	}
+
+	while (list->zac->next != list->potocny)
+		list->zac = list->zac->next;
+
+	list->potocny = list->zac;
+	return 'U';
+}
+
+char lowl_cur_step_right(LOWL* list) {
+	if (list == NULL || list->zac == NULL || list->potocny == NULL || list->potocny->next == NULL) {
+		return 'P';//kurzor je už na začiatku alebo je zoznam prázdny
+	}
+
+	list->potocny = list->potocny->next;
+
+	return 'U';
+}
+
+OWN* lowl_insert_left(LOWL* list, float hodnota) {
+	if (list == NULL)
+	{
+		return NULL;
+	}
+
+	OWN* newuzol = (OWN*)malloc(sizeof(OWN));
+	if (newuzol == NULL) {
+		return NULL;
+	}
+
+	newuzol->data = hodnota;
+	if (list->zac == NULL) {
+		list->zac = newuzol;
+	}
+	else if (list->potocny == list->zac) {
+		newuzol->next = list->zac;
+		list->zac = newuzol;
+	}
+	else {
+		while (list->zac != list->potocny)
+			list->zac = list->zac->next;
+
+		newuzol->next = list->potocny;
+		list->zac = newuzol;
+	}
+	return newuzol;
+}
+
+OWN* lowl_insert_right(LOWL* list, float hodnota) {
+	if (list == NULL)
+	{
+		return NULL;
+	}
+
+	OWN* newuzol = (OWN*)malloc(sizeof(OWN));
+	if (newuzol == NULL) {
+		return NULL;
+	}
+
+	newuzol->data = hodnota;
+	if (list->zac == NULL) {
+		list->zac = newuzol;
+	}
+	else {
+		newuzol->next = list->potocny->next;
+		list->potocny->next = newuzol;
+	}
+	return newuzol;
+}
+
 int main() {
 
 	return 0;
