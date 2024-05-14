fba62b661ea94663f486b9fa5f91b1048fff7bd0
Ivan Bilyk <thinkingyyy@gmail.com>
2024-05-14 21:39:42 +0300

    Vytvorenie štruktúry OWN, ktorá obsahuje dátové pole, do ktorého sa uloží číselná hodnota uzla, a pole next, ktoré označuje ďalší uzol v zozname.

---
+++ Zadania11.4.8/Source.cpp
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct OWN{
+	float data;
+	struct OWN* next;
+}OWN;
+
+typedef struct {
+
+}LOWL;
+
+char lowl_interpolate_linear(LOWL* list) {
+
+}
+
+int main() {
+
+	return 0;
+}
