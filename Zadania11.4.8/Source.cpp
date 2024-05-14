dd233ef46f86a57cb6be83147e5d53d5faa0ec67
Ivan Bilyk <thinkingyyy@gmail.com>
2024-05-14 22:05:08 +0300

    Vytvorenie funkcie na interpoláciu uzlov. Ak zoznam nie je prázdny a obsahuje aspoň dve hodnoty, prejde celý zoznam a pre každé dve hodnoty vytvorí interpolovanú hodnotu, ktorá je priemernou hodnotou. Na konci funkcia vráti hodnotu „U“, čo znamená úspech.

--- Zadania11.4.8/Source.cpp
+++ Zadania11.4.8/Source.cpp
@@ -1,4 +1,4 @@
-#include "stdio.h"
+﻿#include "stdio.h"
 #include "stdlib.h"
 
 typedef struct OWN{
@@ -12,7 +12,24 @@ typedef struct {
 }LOWL;
 
 char lowl_interpolate_linear(LOWL* list) {
+	if (list == NULL || list->zac == NULL || list->zac->next == NULL) {
+		return 'N';//nedostatok uzlov na interpoláciu
+	}
 
+	OWN* curr = list->zac;
+	while (curr->next != NULL) {
+		float interpolated = (curr->data + curr->next->data) / 2;
+
+		OWN* newuzol = (OWN*)malloc(sizeof(OWN));
+
+		newuzol->data = interpolated;
+		newuzol->next = curr->next;
+		curr->next = newuzol;
+
+		curr = curr->next->next;
+	}
+
+	return 'U';//Uspeh
 }
 
 int main() {
